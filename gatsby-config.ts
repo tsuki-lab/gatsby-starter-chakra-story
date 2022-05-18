5e861a89a765b1890524ab037afc17065c5e6e04
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-05-18 22:23:57 +0900

    Adapt rfc typegen

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,22 +1,52 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby'
+
+require('dotenv').config()
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Example GatsbyJS 2022`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
+    itemsLimit: 10
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    'gatsby-plugin-pnpm',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png'
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
+      }
+    },
+    {
+      resolve: 'gatsby-source-microcms',
+      options: {
+        apiKey: process.env.MICROCMS_API_KEY,
+        serviceId: process.env.MICROCMS_SERVICE_ID,
+        apis: [
+          {
+            endpoint: 'blogs'
+          },
+          {
+            endpoint: 'categories'
+          }
+        ]
+      }
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    }
-  }, "gatsby-plugin-pnpm"]
-};
+  ],
+  flags: {
+    GRAPHQL_TYPEGEN: true
+  }
+}
 
-export default config;
+export default config
